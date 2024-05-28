b16d21790c76d4730792a89a95ec22fb185c650f
mneeleman <neeleman@mpia.de>
2024-05-28 13:02:38 -0400

    fix bug in standard naming convention cflag_output

--- cflag.py
+++ cflag.py
@@ -24,7 +24,7 @@ def cflag_output(pl_dir, proj_dir, to_jsonfile=False, jsonfile=None, get_imagest
         return strct
     else:
         if jsonfile is None:
-            jsonfile = strct['proposal_code'] + '_' + '_'.join(strct['mous_uid'].split('/')) + '.json'
+            jsonfile = strct['proposal_code'] + '_' + '_'.join(strct['mous_uid'].split(':/')) + '.json'
         with open(jsonfile, 'w') as fp:
             json.dump(strct, fp)
 
